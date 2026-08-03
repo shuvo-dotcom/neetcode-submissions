411a2ba14a298c0b5e581f2f85c24c74ded2574b
Suvajit Lodh <x23356987@student.ncirl.ie>
2026-08-03 18:14:22 +0100

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        dict_s = {}
+        dict_t = {}
+        for i in s:
+            if i in dict_s: dict_s[i]+=1
+            else: dict_s[i] = 1
+        for j in t:
+            if j in dict_t: dict_t[j]+=1
+            else: dict_t[j] = 1
+        return dict_s==dict_t
